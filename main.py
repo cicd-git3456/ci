d0e912488f142b43b186e569e86d217fde386cbd
mhaque04 <rizwanhrizvi@gmail.com>
2026-08-19 01:18:30 -0400

    commit 17

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 
-sleep_time = 557
-print(f"Commit 16: sleeping for {sleep_time} seconds...")
+sleep_time = 600
+print(f"Commit 17: sleeping for {sleep_time} seconds...")
 time.sleep(sleep_time)
 print("Done!")
